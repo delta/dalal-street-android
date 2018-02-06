c5be612582b74ad374512dd7484a018b5ce8a47e
Santhosh Kumar <santhoshsamy29@gmail.com>
2018-02-06 20:33:05 +0530

    Add Notif Pagination

--- app/src/main/java/com/hmproductions/theredstreet/notifications/NotificationFragment.java
+++ app/src/main/java/com/hmproductions/theredstreet/notifications/NotificationFragment.java
@@ -2,6 +2,7 @@ package com.hmproductions.theredstreet.notifications;
 
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,7 +12,6 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +27,6 @@ import com.hmproductions.theredstreet.utils.ConnectionUtils;
 import com.hmproductions.theredstreet.utils.Constants;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.inject.Inject;
 
@@ -38,11 +37,16 @@ import dalalstreet.api.actions.GetNotificationsResponse;
 
 public class NotificationFragment extends Fragment implements LoaderManager.LoaderCallbacks<GetNotificationsResponse> {
 
+    private static final String LAST_NOTIFICATION_ID = "last_notification_id";
+
     @Inject
     DalalActionServiceGrpc.DalalActionServiceBlockingStub actionServiceBlockingStub;
 
     @Inject
     NotificationRecyclerAdapter notificationRecyclerAdapter;
+
+    @Inject
+    SharedPreferences preferences;
 
     @BindView(R.id.notifications_recyclerView)
     RecyclerView notificationsRecyclerView;
@@ -52,6 +56,9 @@ public class NotificationFragment extends Fragment implements LoaderManager.Load
 
     private ConnectionUtils.OnNetworkDownHandler networkDownHandler;
     private AlertDialog loadingDialog;
+
+    boolean paginate = true;
+    ArrayList<Notification> customNotificationList = new ArrayList<>();
 
     public NotificationFragment() {
         // Required empty public constructor
@@ -71,7 +78,7 @@ public class NotificationFragment extends Fragment implements LoaderManager.Load
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getContext() != null){
+        if (getContext() != null) {
             View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.progress_dialog, null);
             String tempString = "Getting notifications...";
             ((TextView) dialogView.findViewById(R.id.progressDialog_textView)).setText(tempString);
@@ -91,21 +98,34 @@ public class NotificationFragment extends Fragment implements LoaderManager.Load
         if (getActivity() != null) getActivity().setTitle(getString(R.string.notifications));
         DaggerDalalStreetApplicationComponent.builder().contextModule(new ContextModule(getContext())).build().inject(this);
 
+        notificationRecyclerAdapter = new NotificationRecyclerAdapter(getContext(), null);
         notificationsRecyclerView.setAdapter(notificationRecyclerAdapter);
         notificationsRecyclerView.setHasFixedSize(false);
+        notificationsRecyclerView.addOnScrollListener(new CustomScrollListener());
         notificationsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         getActivity().getSupportLoaderManager().restartLoader(Constants.NOTIFICATION_LOADER_ID, null, this);
+        loadingDialog.show();
 
         return rootView;
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        preferences
+                .edit()
+                .remove(LAST_NOTIFICATION_ID)
+                .apply();
+
+    }
+
+    @Override
     public Loader<GetNotificationsResponse> onCreateLoader(int id, Bundle args) {
-        loadingDialog.show();
 
         if (getContext() != null) {
-            return new NotificationLoader(getContext(), actionServiceBlockingStub);
+            int lastId = preferences.getInt(LAST_NOTIFICATION_ID, 0);
+            return new NotificationLoader(getContext(), actionServiceBlockingStub, lastId);
         } else {
             return null;
         }
@@ -121,13 +141,15 @@ public class NotificationFragment extends Fragment implements LoaderManager.Load
             return;
         }
 
-        if (data.getNotificationsList().size() > 0) {
+        paginate = data.getNotificationsCount() == 10;
 
-            List<Notification> customNotificationList = new ArrayList<>();
-            customNotificationList.clear();
+        if (data.getNotificationsList().size() > 0) {
 
             for (dalalstreet.api.models.Notification currentNotification : data.getNotificationsList()) {
                 customNotificationList.add(new Notification(currentNotification.getText(), currentNotification.getCreatedAt()));
+                preferences.edit()
+                        .putInt(LAST_NOTIFICATION_ID, currentNotification.getId())
+                        .apply();
             }
 
             notificationRecyclerAdapter.swapData(customNotificationList);
@@ -143,5 +165,34 @@ public class NotificationFragment extends Fragment implements LoaderManager.Load
     @Override
     public void onLoaderReset(Loader<GetNotificationsResponse> loader) {
         // Do nothing
+    }
+
+
+    public class CustomScrollListener extends RecyclerView.OnScrollListener {
+
+        CustomScrollListener() {
+
+        }
+
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            int visibleItemCount = recyclerView.getLayoutManager().getChildCount();
+            int totalItemCount = recyclerView.getLayoutManager().getItemCount();
+            int pastVisibleItems = ((LinearLayoutManager) recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
+            if (pastVisibleItems + visibleItemCount >= totalItemCount) {
+
+                if (paginate) {
+                    if (getActivity() != null) {
+                        getActivity().getSupportLoaderManager().restartLoader(Constants.NOTIFICATION_LOADER_ID, null, NotificationFragment.this);
+                        paginate = false;
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        preferences.edit().remove(LAST_NOTIFICATION_ID).apply();
     }
 }
