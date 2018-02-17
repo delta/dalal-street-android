dc4a1552e2600a86bde24eeb20c9becbb3d4d2dd
Santhosh Kumar <santhoshsamy29@gmail.com>
2018-02-17 21:07:06 +0530

    Fix Group Notifications

--- app/src/main/java/com/hmproductions/theredstreet/notifications/NotificationBroadcastReceiver.java
+++ app/src/main/java/com/hmproductions/theredstreet/notifications/NotificationBroadcastReceiver.java
@@ -3,11 +3,13 @@ package com.hmproductions.theredstreet.notifications;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class NotificationBroadcastReceiver extends BroadcastReceiver{
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.e("SAN","broadcast called");
         context.startService(new Intent(context,NotificationService.class));
     }
 }
