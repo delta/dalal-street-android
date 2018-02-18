ea960e31de08a01cac5d1dcdc039be08853246b3
Santhosh Kumar <santhoshsamy29@gmail.com>
2018-02-18 11:54:33 +0530

    Add Help and remove Logs

--- app/src/main/java/com/hmproductions/theredstreet/notifications/NotificationBroadcastReceiver.java
+++ app/src/main/java/com/hmproductions/theredstreet/notifications/NotificationBroadcastReceiver.java
@@ -3,13 +3,11 @@ package com.hmproductions.theredstreet.notifications;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class NotificationBroadcastReceiver extends BroadcastReceiver{
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.e("SAN","broadcast called");
         context.startService(new Intent(context,NotificationService.class));
     }
 }
