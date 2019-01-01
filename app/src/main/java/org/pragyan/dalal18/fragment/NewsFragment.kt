f2f817d5861560d5cba6197aa975038d2ce1a91b
Harsh2098 <harshmahajan927@yahoo.com>
2019-01-02 00:15:19 +0800

    Add swipe refresh in news fragment
    Changed news fragment layout to constraint layout

--- app/src/main/java/org/pragyan/dalal18/fragment/NewsFragment.kt
+++ app/src/main/java/org/pragyan/dalal18/fragment/NewsFragment.kt
@@ -4,18 +4,17 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import dalalstreet.api.DalalActionServiceGrpc
 import dalalstreet.api.actions.GetMarketEventsRequest
 import kotlinx.android.synthetic.main.fragment_news.*
@@ -29,11 +28,10 @@ import org.pragyan.dalal18.data.NewsDetails
 import org.pragyan.dalal18.ui.NewsDetailsActivity
 import org.pragyan.dalal18.utils.ConnectionUtils
 import org.pragyan.dalal18.utils.Constants
-import java.util.ArrayList
 import javax.inject.Inject
 
 
-class NewsFragment : Fragment(),NewsRecyclerAdapter.NewsItemClickListener {
+class NewsFragment : Fragment(), NewsRecyclerAdapter.NewsItemClickListener, SwipeRefreshLayout.OnRefreshListener {
 
     @Inject
     lateinit var actionServiceBlockingStub: DalalActionServiceGrpc.DalalActionServiceBlockingStub
@@ -41,101 +39,75 @@ class NewsFragment : Fragment(),NewsRecyclerAdapter.NewsItemClickListener {
     private var newsRecyclerAdapter: NewsRecyclerAdapter? = null
 
     private var loadingNewsDialog: AlertDialog? = null
-    private var newsDetailsList: List<NewsDetails> = ArrayList()
+
+    private var newsDetailsList = mutableListOf<NewsDetails>()
 
     lateinit var networkDownHandler: ConnectionUtils.OnNetworkDownHandler
-
-
 
     private val refreshNewsListListener = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             if (intent.action != null && intent.action.equals(Constants.REFRESH_NEWS_ACTION, ignoreCase = true))
-                getNewsAsync();
+                getNewsAsynchronously()
         }
     }
 
-    private fun getNewsAsync() {
+    private fun getNewsAsynchronously() {
 
         loadingNewsDialog?.show()
-
 
         doAsync {
 
             if (ConnectionUtils.getConnectionInfo(context!!) && ConnectionUtils.isReachableByTcp(Constants.HOST, Constants.PORT)) {
 
-                val newsList = ArrayList<NewsDetails>()
-
                 val marketEventsResponse = actionServiceBlockingStub.getMarketEvents(
                         GetMarketEventsRequest.newBuilder().setCount(0).setLastEventId(0).build())
 
                 uiThread {
+                    loadingNewsDialog?.dismiss()
+                    newsSwipeRefreshLayout.isRefreshing = false
+
                     if (marketEventsResponse.statusCode.number == 0) {
 
-                        newsList.clear()
+                        newsDetailsList.clear()
 
                         for (currentMarketEvent in marketEventsResponse.marketEventsList) {
-                            newsList.add(NewsDetails(currentMarketEvent.createdAt, currentMarketEvent.headline,
+                            newsDetailsList.add(NewsDetails(currentMarketEvent.createdAt, currentMarketEvent.headline,
                                     currentMarketEvent.text, currentMarketEvent.imagePath))
                         }
 
-
-
-                        loadingNewsDialog?.dismiss()
-
-                        if (newsList.size != 0) {
-                            newsDetailsList =  newsList
+                        if (newsDetailsList.size != 0) {
                             newsRecyclerAdapter?.swapData(newsDetailsList)
-                            noNews_textView.visibility = View.GONE
+                            noNewsTextView.visibility = View.GONE
                             newsRecyclerView.visibility = View.VISIBLE
                         } else {
-                            noNews_textView.visibility = View.VISIBLE
+                            noNewsTextView.visibility = View.VISIBLE
                             newsRecyclerView.visibility = View.GONE
                         }
 
                     } else {
-
                         Toast.makeText(context, "Server Error", Toast.LENGTH_SHORT).show()
                     }
-
                 }
-
             } else {
                 uiThread { networkDownHandler.onNetworkDownError() }
             }
         }
-
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-
         try {
             networkDownHandler = context as ConnectionUtils.OnNetworkDownHandler
         } catch (classCastException: ClassCastException) {
             throw ClassCastException(context.toString() + " must implement network down handler.")
         }
-
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val rootView = inflater.inflate(R.layout.fragment_news, container, false)
-
-        if (activity != null) activity!!.title = "News"
-
         DaggerDalalStreetApplicationComponent.builder().contextModule(ContextModule(context!!)).build().inject(this)
-
-
-
-        if (context != null) {
-            val dialogView = LayoutInflater.from(context).inflate(R.layout.progress_dialog, null)
-            (dialogView.findViewById<View>(R.id.progressDialog_textView) as TextView).setText(R.string.getting_latest_news)
-            loadingNewsDialog = AlertDialog.Builder(context!!).setView(dialogView).setCancelable(false).create()
-        }
-
         return rootView
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -143,29 +115,36 @@ class NewsFragment : Fragment(),NewsRecyclerAdapter.NewsItemClickListener {
 
         newsRecyclerAdapter = NewsRecyclerAdapter(context, null, this)
 
-        newsRecyclerView.setLayoutManager(LinearLayoutManager(context))
-        newsRecyclerView.setHasFixedSize(false)
-        newsRecyclerView.setAdapter(newsRecyclerAdapter)
+        with(newsRecyclerView) {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(false)
+            adapter = newsRecyclerAdapter
+        }
 
-        getNewsAsync()
+        newsSwipeRefreshLayout.setOnRefreshListener(this)
+
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.progress_dialog, null)
+        (dialogView.findViewById<View>(R.id.progressDialog_textView) as TextView).setText(R.string.getting_latest_news)
+        loadingNewsDialog = AlertDialog.Builder(context!!).setView(dialogView).setCancelable(false).create()
+
+        getNewsAsynchronously()
     }
 
     override fun onResume() {
         super.onResume()
-        LocalBroadcastManager.getInstance(context!!)
-                .registerReceiver(refreshNewsListListener, IntentFilter(Constants.REFRESH_NEWS_ACTION))
+        LocalBroadcastManager.getInstance(context!!).registerReceiver(refreshNewsListListener, IntentFilter(Constants.REFRESH_NEWS_ACTION))
     }
 
     override fun onPause() {
         super.onPause()
-        LocalBroadcastManager.getInstance(context!!)
-                .unregisterReceiver(refreshNewsListListener)
+        LocalBroadcastManager.getInstance(context!!).unregisterReceiver(refreshNewsListListener)
     }
+
+    override fun onRefresh() = getNewsAsynchronously()
 
     override fun onNewsClicked(view: View?, position: Int) {
         val intent = Intent(context, NewsDetailsActivity::class.java)
         intent.putExtra(NewsDetailsActivity.NEWS_DETAILS_KEY, newsDetailsList[position])
         startActivity(intent)
     }
-
 }
