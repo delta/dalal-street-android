2533a0afe91d18cdccf187a3805d4d081e661f6b
Santhosh Kumar M <santhoshsamy29@gmail.com>
2019-01-10 20:20:16 +0530

    Add PieChart in WorthFragment

--- app/src/main/java/org/pragyan/dalal18/dagger/DalalStreetApplicationComponent.java
+++ app/src/main/java/org/pragyan/dalal18/dagger/DalalStreetApplicationComponent.java
@@ -9,6 +9,7 @@ import org.pragyan.dalal18.fragment.PortfolioFragment;
 import org.pragyan.dalal18.fragment.StockExchangeFragment;
 import org.pragyan.dalal18.fragment.TradeFragment;
 import org.pragyan.dalal18.fragment.TransactionsFragment;
+import org.pragyan.dalal18.fragment.WorthFragment;
 import org.pragyan.dalal18.fragment.marketDepth.DepthGraphFragment;
 import org.pragyan.dalal18.fragment.marketDepth.DepthTableFragment;
 import org.pragyan.dalal18.notifications.NotificationFragment;
@@ -44,6 +45,7 @@ public interface DalalStreetApplicationComponent {
     void inject(OrdersFragment ordersFragment);
     void inject(PortfolioFragment portfolioFragment);
     void inject(NotificationFragment notificationFragment);
+    void inject(WorthFragment worthFragment);
 
     void inject(NotificationService notificationService);
 }
