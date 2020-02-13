6b7661eadcb4481c4ad81048d9e9b621a0eefa39
Vivek <satsvivek@gmail.com>
2020-02-13 16:47:05 +0530

    Create norificationservice

---
+++ app/src/main/java/org/pragyan/dalal18/notifications/PushNotificationService.kt
@@ -0,0 +1,12 @@
+package org.pragyan.dalal18.notifications
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class PushNotificationService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
