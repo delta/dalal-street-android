c63628525179c5ffa56ec89e9eda7275e1f81fe4
Avi Kumar Singh <avithewinner1508@gmail.com>
2020-02-24 23:13:32 +0000

    Infinite companies in recycler

--- app/src/main/java/org/pragyan/dalal18/utils/Constants.java
+++ app/src/main/java/org/pragyan/dalal18/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     // TODO (Release): Change server IP and Port
 
-    public static final String HOST = "192.168.0.50";
+    public static final String HOST = "delta.nitt.edu";
 
     public static final int PORT = 8000;
 
