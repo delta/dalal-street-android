392e7a8ca418c33d70fb9e4f09b34efe1003016a
Aditya Rana <42575044+ranaaditya@users.noreply.github.com>
2021-01-19 08:49:57 +0530

    Replacing Koltin-synthetics with view bindings (#177)
    * Upgrade Build System to support ViewBinding
    * Ignore ViewBinding in fragment_getting_started.xml, fragment_faq.xml to avoid Build Errors
    * Add ViewBinding in Activities
    * Add property delegate to handle ViewBinding lifecycle for Activity and Fragment
    * Add ViewBinding property delegate in Activities
    * Add ViewBinding in HelpMainFragment.kt
    * Add ViewBinding in MarketDepthFragment.kt
    * Add ViewBinding in DepthGraphFragment.kt, DepthTableFragment.kt
    * Add ViewBinding in MainMortgageFragment.kt
    * Add ViewBinding in MortgageFragment.kt, RetrieveFragment.kt
    * Add ViewBinding in LeaderboardFragment.kt
    * Add ViewBinding in HomeFragment.kt
    * Add ViewBinding in CompanyFragment.kt, CompanyDescriptionFragment.kt
    * Add ViewBinding in PortfolioFragment.kt
    * Add ViewBinding in StockExchangeFragment.kt
    * Add ViewBinding in TradeFragment.kt
    * Add ViewBinding in NewsFragment.kt
    * Add ViewBinding in OrdersFragment.kt
    * Add ViewBinding in TransactionsFragment.kt
    * Add ViewBinding in AddPhoneFragment.kt, OTPVerificationFragment.kt
    * Add ViewBinding in NewsDetailsFragment.kt
    * Add ViewBinding in NotificationFragment.kt
    * This commit fixes issue #159
    
    Co-authored-by: Sudhindra Dondapati  <sudhindra5678@gmail.com>

--- app/src/main/java/org/pragyan/dalal18/fragment/help/HelpMainFragment.kt
+++ app/src/main/java/org/pragyan/dalal18/fragment/help/HelpMainFragment.kt
@@ -8,24 +8,30 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_main_help.*
 import org.pragyan.dalal18.R
 import org.pragyan.dalal18.adapter.pagerAdapters.HelpPagerAdapter
+import org.pragyan.dalal18.databinding.FragmentMainHelpBinding
+import org.pragyan.dalal18.utils.viewLifecycle
 
-class HelpMainFragment : Fragment(){
+class HelpMainFragment : Fragment() {
+
+    private var binding by viewLifecycle<FragmentMainHelpBinding>()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val customView = inflater.inflate(R.layout.fragment_main_help, container, false)
-        return customView
+        binding = FragmentMainHelpBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.title = resources.getString(R.string.help_title)
 
-        helpViewPager.adapter = HelpPagerAdapter(childFragmentManager)
-        mainHelpTabLayout.setupWithViewPager(helpViewPager)
+        binding.apply {
+            helpViewPager.adapter = HelpPagerAdapter(childFragmentManager)
+            mainHelpTabLayout.setupWithViewPager(helpViewPager)
 
-        mainHelpTabLayout.setTabTextColors(ContextCompat.getColor(context!!, R.color.neutral_font_color), ContextCompat.getColor(context!!, R.color.neon_blue))
-        mainHelpTabLayout.setBackgroundColor(Color.parseColor("#20202C"))
+            mainHelpTabLayout.setTabTextColors(ContextCompat.getColor(context!!, R.color.neutral_font_color), ContextCompat.getColor(context!!, R.color.neon_blue))
+            mainHelpTabLayout.setBackgroundColor(Color.parseColor("#20202C"))
+        }
     }
 }
