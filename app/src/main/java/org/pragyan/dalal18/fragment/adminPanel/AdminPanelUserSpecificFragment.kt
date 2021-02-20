6070d77e5c84158c5eec42268fcacf4c347410a6
satyamurti <spd.doddini@gmail.com>
2021-02-20 22:31:54 +0530

    Added 3 fragments for TabLayout

--- app/src/main/java/org/pragyan/dalal18/fragment/adminPanel/AdminPanelUserSpecificFragment.kt
+++ app/src/main/java/org/pragyan/dalal18/fragment/adminPanel/AdminPanelUserSpecificFragment.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import dalalstreet.api.DalalActionServiceGrpc
 import dalalstreet.api.actions.*
+import kotlinx.android.synthetic.main.fragment_adminpanel_userspecific.*
 import kotlinx.android.synthetic.main.fragment_getting_started.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -54,5 +55,70 @@ class AdminPanelUserSpecificFragment : Fragment() {
         }
 
     }
+
+    private fun blockUser() = lifecycleScope.launch {
+        view?.hideKeyboard()
+        withContext(Dispatchers.IO) {
+            if (ConnectionUtils.getConnectionInfo(context!!) && ConnectionUtils.isReachableByTcp(Constants.HOST, Constants.PORT)) {
+
+                if (blockuserEditText.text.toString().isNotBlank()
+                ) {
+                    val response = actionServiceBlockingStub.blockUser(BlockUser.BlockUserRequest.newBuilder()
+                            .setUserId(blockuserEditText.text.toString().toInt())
+                            .build())
+                    message = response.statusMessage
+                }
+            }
+        }
+        context?.toast(message)
+    }
+    private fun unblockUser() = lifecycleScope.launch {
+        view?.hideKeyboard()
+        withContext(Dispatchers.IO) {
+            if (ConnectionUtils.getConnectionInfo(context!!) && ConnectionUtils.isReachableByTcp(Constants.HOST, Constants.PORT)) {
+
+                if (unblockuserEditText.text.toString().isNotBlank()
+                ) {
+                    val response = actionServiceBlockingStub.unBlockUser(UnblockUser.UnblockUserRequest.newBuilder()
+                            .setUserId(unblockuserEditText.text.toString().toInt())
+                            .build())
+                    message = response.statusMessage
+                }
+            }
+        }
+        context?.toast(message)
+    }
+    private fun unblockAllUser() = lifecycleScope.launch {
+        view?.hideKeyboard()
+        withContext(Dispatchers.IO) {
+            if (ConnectionUtils.getConnectionInfo(context!!) && ConnectionUtils.isReachableByTcp(Constants.HOST, Constants.PORT)) {
+                    val response = actionServiceBlockingStub.unBlockAllUsers(UnblockAllUsers.UnblockAllUsersRequest.getDefaultInstance())
+                    message = response.statusMessage
+
+            }
+        }
+        context?.toast(message)
+    }
+    private fun sendNotification() = lifecycleScope.launch {
+        view?.hideKeyboard()
+
+        withContext(Dispatchers.IO) {
+            if (ConnectionUtils.getConnectionInfo(context!!) && ConnectionUtils.isReachableByTcp(Constants.HOST, Constants.PORT)) {
+                binding.apply {
+                    if (userIdforNotification.text.toString().isNotBlank() && notificationEditText.text.toString().isNotBlank()) {
+                        val response = actionServiceBlockingStub.sendNotifications(SendNotifications.SendNotificationsRequest.newBuilder()
+                                .setText(notificationEditText.text.toString())
+                                .setUserId(userIdforNotification.text.toString().toInt())
+                                .setIsGlobal(isGlobalSwitch.isChecked)
+                                .build())
+
+                        message = response.statusMessage
+                    }
+                }
+            }
+        }
+        context?.toast(message)
+    }
+
 
 }
