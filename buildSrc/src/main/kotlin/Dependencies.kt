0dc84b5f033c592d70319481156c35fecde670a9
avikumar15 <avithewinner1508@gmail.com>
2021-04-03 21:19:26 +0530

    chore: bump to version 7.4 :rocket:

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,8 +3,8 @@ object AndroidSdk {
     const val minSdk = 21
     const val targetSdk = 29
 
-    const val versionCode = 19
-    const val versionName = "7.3"
+    const val versionCode = 20
+    const val versionName = "7.4"
 
     const val buildToolsVersion = "29.0.3"
     const val DalalAppId = "org.pragyan.dalal18"
