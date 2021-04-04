56c8ab8126fe26607f2841f44eb47094fcc9c548
Satyamurti <spd.doddini@gmail.com>
2021-04-04 19:42:17 +0530

    fixed notification in fcm

--- app/src/main/java/org/pragyan/dalal18/fcm/FCMService.kt
+++ app/src/main/java/org/pragyan/dalal18/fcm/FCMService.kt
@@ -1,14 +1,17 @@
 package org.pragyan.dalal18.fcm
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.Intent
 import android.media.RingtoneManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import org.pragyan.dalal18.R
+import org.pragyan.dalal18.ui.SplashActivity
 
 class FCMService: FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
@@ -19,10 +22,10 @@ class FCMService: FirebaseMessagingService() {
         Log.d("FCM-MESSAGE", remoteMessage.notification?.body.toString())
     }
     private fun sendNotification(remoteMessage: RemoteMessage) {
-//        val intent = Intent(this, MainActivity::class.java)
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-//        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-//                PendingIntent.FLAG_ONE_SHOT)
+        val intent = Intent(this, SplashActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this)
                 .setContentText(remoteMessage.notification?.body)
@@ -30,6 +33,7 @@ class FCMService: FirebaseMessagingService() {
                 .setContentTitle(remoteMessage.notification?.title)
                 .setSmallIcon(R.mipmap.dalal_icon21)
                 .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
